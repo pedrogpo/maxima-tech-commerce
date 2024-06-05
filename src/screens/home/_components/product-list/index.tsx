8f3d780254fa7ab677affdd5eeca7be14614643f
Pedro Guilherme <ploowcs@gmail.com>
2024-06-05 13:16:46 -0300

    fix: Products was not using as an async component

--- src/screens/home/_components/product-list/index.tsx
+++ src/screens/home/_components/product-list/index.tsx
@@ -2,12 +2,12 @@ import CategoryList from './category-list'
 import { ListHeader } from './header'
 import Products from './products'
 
-export default function ProductList() {
+export default async function ProductList() {
   return (
     <div className="mb-32 mt-16">
       <ListHeader />
       <CategoryList />
-      <Products />
+      {await Products()}
     </div>
   )
 }
