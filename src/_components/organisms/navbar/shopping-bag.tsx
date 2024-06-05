ddff4b970f2b0584d8e12351a00ee68d96d92847
Pedro Guilherme <ploowcs@gmail.com>
2024-06-05 17:53:22 -0300

    chore: add redirect to cart page in shopping bag

--- src/_components/organisms/navbar/shopping-bag.tsx
+++ src/_components/organisms/navbar/shopping-bag.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { observer } from 'mobx-react-lite'
+import Link from 'next/link'
 import { BiShoppingBag } from 'react-icons/bi'
 import { Button } from '~/_components/ui/button'
 import useCart from '~/hooks/useCart'
@@ -9,15 +10,17 @@ export const ShoppingBag = observer(() => {
   const { currentItems } = useCart()
 
   return (
-    <Button variant="ghost" aria-label="Carrinho">
-      <div className="relative">
-        <BiShoppingBag size={24} />
-        {currentItems.length > 0 && (
-          <div className="absolute -right-1 -top-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-white">
-            {currentItems.length}
-          </div>
-        )}
-      </div>
-    </Button>
+    <Link href="/cart">
+      <Button variant="ghost" aria-label="Carrinho">
+        <div className="relative">
+          <BiShoppingBag size={24} />
+          {currentItems.length > 0 && (
+            <div className="absolute -right-1 -top-1 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-white">
+              {currentItems.length}
+            </div>
+          )}
+        </div>
+      </Button>
+    </Link>
   )
 })
